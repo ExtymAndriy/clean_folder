9671b572f591410f3c1bed255df5d39c5debe5f3
ExtymAndriy <150171957+ExtymAndriy@users.noreply.github.com>
2024-01-17 13:33:59 +0200

    first commit

---
+++ clean/main.py
@@ -0,0 +1,16 @@
+import os
+import argparse
+
+def clean_folder(directory):
+    # Реалізація функції очищення папки тут
+    pass
+
+def main():
+    parser = argparse.ArgumentParser(description='Clean a folder.')
+    parser.add_argument('directory', help='Path to the folder to clean')
+    args = parser.parse_args()
+    
+    clean_folder(args.directory)
+
+if __name__ == '__main__':
+    main()
