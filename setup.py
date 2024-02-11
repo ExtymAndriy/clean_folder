200bf4d10c014cf20bd8d82292eed5175266a437
Andruleek <154084991+Andruleek@users.noreply.github.com>
2024-02-11 19:23:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='clean_folder',
-    version='0.1',
+    version='1.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'clean-folder=clean_folder.setup:setup',
+            'clean-folder = clean_folder.clean_folder.clean:main',
         ],
     },
-    install_requires=[
-        # Список залежностей, якщо потрібно
-    ],
 )
 
